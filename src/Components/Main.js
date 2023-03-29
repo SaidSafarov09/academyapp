8190619934ad80289c4bfdf3660ba461438753a8
SaidSafarov09 <93523342+SaidSafarov09@users.noreply.github.com>
2023-03-30 02:58:56 +0300

    [WEBDEV] example

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -109,7 +109,7 @@ function Main() {
                                 Запись на первый день марафона
                             </p>
                             <div className='free'>
-                                <p>FREE</p>
+                                <p>FREEP</p>
                             </div>
                         </div>
                     </div>    
